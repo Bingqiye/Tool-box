65876936bf61cdfd33adfec2cc2992eea0a6142b
Bingqiye <136292118+Bingqiye@users.noreply.github.com>
2023-07-05 13:41:30 +0800

    Update 广义线性模型变量筛选.R

--- 变量筛选/广义线性模型变量筛选.R
+++ 变量筛选/广义线性模型变量筛选.R
@@ -1,20 +1,42 @@
 linear_model_feature_selection <- function(dataframe,outcome,exposure,cov){
-  temp <- dplyr::select(dataframe,c(outcome,cov,exposure))
-  results_summary <- NULL
-  for (i in c((sum(length(outcome),length(cov))+1):ncol(temp))) {
-    temp_glm <- dplyr::select(dataframe,c(outcome,cov,colnames(temp)[i]))
-    fomula <- paste(outcome,"~",".",sep = "")
-    model <- glm(fomula,data = temp_glm)
-    if((length(coef(summary(model)))/4)>(sum(length(outcome),length(cov)))){
-      model_coef <- as.data.frame(t(coef(summary(model))[sum(length(outcome),length(cov))+1,]))
-      model_coef$ID <- colnames(temp)[i]
-      results_summary <- rbind(results_summary,model_coef)
-    }else {
-      model_coef <- data.frame("Estimate"=NA,"Std. Error"=NA,"t value"=NA,"Pr(>|t|)"=NA,"ID"=colnames(temp)[i],check.names = F)
-      results_summary <- rbind(results_summary,model_coef)
+  if(cov!="None"){
+    temp <- dplyr::select(dataframe,c(outcome,cov,exposure))
+    results_summary <- NULL
+    for (i in c((sum(length(outcome),length(cov))+1):ncol(temp))) {
+      temp_glm <- dplyr::select(dataframe,c(outcome,cov,colnames(temp)[i]))
+      fomula <- paste(outcome,"~",".",sep = "")
+      model <- glm(fomula,data = temp_glm)
+      if((length(coef(summary(model)))/4)>(sum(length(outcome),length(cov)))){
+        model_coef <- as.data.frame(t(coef(summary(model))[sum(length(outcome),length(cov))+1,]))
+        model_coef$ID <- colnames(temp)[i]
+        results_summary <- rbind(results_summary,model_coef)
+      }else {
+        model_coef <- data.frame("Estimate"=NA,"Std. Error"=NA,"t value"=NA,"Pr(>|t|)"=NA,"ID"=colnames(temp)[i],check.names = F)
+        results_summary <- rbind(results_summary,model_coef)
+      }
     }
+    results_summary <- results_summary[order(results_summary$`Pr(>|t|)`,decreasing = F),]
+    results_summary$FDR <- p.adjust(results_summary$`Pr(>|t|)`,method = "BH")
+    return(results_summary)
   }
-  results_summary <- results_summary[order(results_summary$`Pr(>|t|)`,decreasing = F),]
-  results_summary$FDR <- p.adjust(results_summary$`Pr(>|t|)`,method = "BH")
-  return(results_summary)
+  else if(cov=="None"){
+    temp <- dplyr::select(dataframe,c(outcome,exposure))
+    results_summary <- NULL
+    for (i in c((length(outcome):ncol(temp)))) {
+      temp_glm <- dplyr::select(dataframe,c(outcome,colnames(temp)[i]))
+      fomula <- paste(outcome,"~",".",sep = "")
+      model <- glm(fomula,data = temp_glm)
+      if((length(coef(summary(model)))/4)>(length(outcome))){
+        model_coef <- as.data.frame(t(coef(summary(model))[length(outcome)+1,]))
+        model_coef$ID <- colnames(temp)[i]
+        results_summary <- rbind(results_summary,model_coef)
+      }else {
+        model_coef <- data.frame("Estimate"=NA,"Std. Error"=NA,"t value"=NA,"Pr(>|t|)"=NA,"ID"=colnames(temp)[i],check.names = F)
+        results_summary <- rbind(results_summary,model_coef)
+      }
+    }
+    results_summary <- results_summary[order(results_summary$`Pr(>|t|)`,decreasing = F),]
+    results_summary$FDR <- p.adjust(results_summary$`Pr(>|t|)`,method = "BH")
+    return(results_summary)
+  }
 }
